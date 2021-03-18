f1d4483c0aadb2b866770cc5a1a80791c638ce35
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-18 13:25:49 -0300

    (styles)🎨: config global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 export const App= () => {
   return (
     <div className="App">
       <h1>World hold </h1>
+      <GlobalStyles/>
     </div>
   );
 };
