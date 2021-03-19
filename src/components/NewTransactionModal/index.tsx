5400297e677224a4cd82945c010161b22ef10d5b
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-18 21:01:14 -0300

    (feat)🧠: edit styles color button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImage from '../../assets/close.svg';
+import incomeImage from '../../assets/income.svg';
+import outcomeImage from '../../assets/outcome.svg';
 
 import * as S from './styles';
 
@@ -13,6 +16,8 @@ export const NewTransactionModal = ({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) => {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -32,6 +37,27 @@ export const NewTransactionModal = ({
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" />
         <input placeholder="Valor" type="number" />
+
+        <S.TrasactionTypeContainer>
+          <S.RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImage} alt="Entrada" /> <span>Entrada</span>
+          </S.RadioBox>
+
+          <S.RadioBox
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImage} alt="Saida" /> <span>Saida</span>
+          </S.RadioBox>
+        </S.TrasactionTypeContainer>
+
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </S.Container>
